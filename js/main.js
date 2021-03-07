91727d27f932afc7d890cfb316f05fe0de5ac2e0
Tixonya <dadlesven@gmail.com>
2021-03-07 18:44:34 +0200

    video in slick

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 const win_height_padded = $(window).height() * 0.8;
 
+$(window).resize(function() {
+    let height = $(".wrapper-video").siblings("img").height()
+    $($(".wrapper-video")[0]).css("height",height);
+    $($(".wrapper-video")[1]).css("height",height);
+});
+    
+
 $(document).ready(function () {
     $('.slider').slick({
         dots: true,
+		autoplay: false,
         arrows: false,
-        autoplay: true,
-        autoplaySpeed: 4000,
     });
 
     $(window).scroll(function () {
@@ -30,21 +36,32 @@ $(document).ready(function () {
         });
     });
 
-    let inputChekbox = $(".checkbox");
-    for (let i = 0; i <= inputChekbox.length; i++){
-        if($(inputChekbox[i]).attr("checked") == 'checked'){
-            $(inputChekbox[i]).parent().css("background-color","#FFC439")
-        }
-    }
+    
+    let height = $(".wrapper-video").siblings("img").height()
+    $($(".wrapper-video")[0]).css("height",height);
+    $($(".wrapper-video")[1]).css("height",height);
 
-    $(".card-bottle-mobile").on("click", function(){
-        let checkbox = $(this).find(".checkbox")[0]
+    $(".checkbox-label").on("click", function(){
+        let url = $(this).parents(".link-a").attr('href')
+        window.location.href = url;
+    });
+
+    for( let i = 0; i < $(".checkbox").length; i++){
+        $(".checkbox")[i].checked = false;
+    }
+    
+
+    
+    $(".card-bottle-mobile").on("click", function(event){
+        let checkbox = $(this).find(".checkbox").prop("checked")
         let $this = $(this);
 
-        if(checkbox.checked){
-            $this.css("background-color","white")
-            $(this).find(".checkbox")[0].checked = false;
-        }else{
+        if(event.target.type !== 'radio'){
+            $($(".card-bottle-mobile")[0]).css("background-color","white");
+            $($(".card-bottle-mobile")[1]).css("background-color","white");
+            $($(".card-bottle-mobile")[2]).css("background-color","white");
+            
+            
             $this.css("background-color","#FFC439")
             $(this).find(".checkbox")[0].checked = true;
         }
@@ -63,26 +80,26 @@ $(document).ready(function () {
     const animationFrequency = 5;
 
     const widgetTexts = [
-        {name: 'Vlad', amount: 3, time: '1 day ago'},
-        {name: 'Alex', amount: 5, time: '2 day ago'},
-        {name: 'Emma', amount: 8, time: '3 day ago'},
-        {name: 'Tony', amount: 8, time: '3 day ago'},
-        {name: 'Bill', amount: 8, time: '1 hour ago'},
-        {name: 'Jack', amount: 8, time: '3 day ago'},
-        {name: 'Sonya', amount: 8, time: '3 day ago'},
-        {name: 'Bob', amount: 8, time: '5 day ago'},
-        {name: 'Jose', amount: 8, time: '3 day ago'},
-        {name: 'Anna', amount: 8, time: '3 day ago'},
+        {name: 'Vlad', amount: 3, time: 'vor 1 Minute'},
+        {name: 'Alex', amount: 5, time: 'vor 6 Minuten'},
+        {name: 'Emma', amount: 8, time: 'vor 1 Stunde'},
+        {name: 'Tanja', amount: 8, time: 'vor 17 Minuten'},
+        {name: 'Benjamin', amount: 8, time: 'vor 23 Minuten'},
+        {name: 'Erwin', amount: 8, time: 'vor 25 Minuten'},
+        {name: 'Sandro', amount: 8, time: 'vor 27 Minuten'},
+        {name: 'Andreas', amount: 8, time: 'vor 32 Minuten'},
+        {name: 'Johannes', amount: 8, time: 'vor 1 Stunde'},
+        {name: 'Anna', amount: 8, time: 'vor 20 Minuten'},
         ];
 
     const renderWidget = ({name, amount, time}) => {
         return `<a class="bought-widget-card fadeIn animated">
                     <div class="bought-widget-card__image">
-                        <img src="img/bottle.png" />                    
+                        <img src="img/1.png" />                    
                     </div>
                      <div class="bought-widget-card__text">
                         <div class="text__name">${name}</div>
-                          <div class="text__message">bought ${amount} superCOAT </div>
+                          <div class="text__message">kaufte gerade ${amount} SuperCoat Set</div>
                            <div class="text__time">${time}</div>
                      </div>
                 </a>`;
